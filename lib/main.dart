16a99f5114b5b693ec3760d0e4015e221be44574
gs <gs@xyz.de>
2025-07-25 16:15:14 +0200

    added slider for luxValue

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  double luxValue = 100;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,7 +43,19 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[_getRadialGauge()],
+          children: <Widget>[
+            _getRadialGauge(),
+            Slider(
+              min: 0,
+              max: 2000,
+              value: luxValue,
+              onChanged: (value) {
+                setState(() {
+                  luxValue = value;
+                });
+              },
+            ),
+          ],
         ),
       ),
     );
@@ -50,44 +64,42 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget _getRadialGauge() {
     return SfRadialGauge(
       title: GaugeTitle(
-        text: 'Speedometer',
+        text: '',
         textStyle: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
       ),
       axes: <RadialAxis>[
         RadialAxis(
           minimum: 0,
-          maximum: 150,
+          maximum: 1001,
           ranges: <GaugeRange>[
             GaugeRange(
               startValue: 0,
-              endValue: 50,
-              color: Colors.green,
+              endValue: 300,
+              color: Colors.red,
               startWidth: 10,
               endWidth: 10,
             ),
             GaugeRange(
-              startValue: 50,
-              endValue: 100,
+              startValue: 300,
+              endValue: 600,
               color: Colors.orange,
               startWidth: 10,
               endWidth: 10,
             ),
             GaugeRange(
-              startValue: 100,
-              endValue: 150,
-              color: Colors.red,
+              startValue: 600,
+              endValue: 1000,
+              color: Colors.green,
               startWidth: 10,
               endWidth: 10,
             ),
           ],
-          pointers: <GaugePointer>[NeedlePointer(value: 90)],
+          pointers: <GaugePointer>[NeedlePointer(value: luxValue)],
           annotations: <GaugeAnnotation>[
             GaugeAnnotation(
-              widget: Container(
-                child: const Text(
-                  '90.0',
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                ),
+              widget: Text(
+                luxValue.toStringAsFixed(0),
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
               ),
               angle: 90,
               positionFactor: 0.5,
